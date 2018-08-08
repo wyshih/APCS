a030b1db3c4972f7b0287b07ac70ef0887090b8e
wyshih <wyshih@cs.nctu.edu.tw>
2018-08-08 14:55:42 +0800

    add comments

--- 1050305-1.py
+++ 1050305-1.py
@@ -1,36 +1,36 @@
 a = input('Please enter the number of students: ')
 try:
-    a = int(a)
+    a = int(a)  #number of students
 except:
     print ('Enter a number, you FOOL!')
 b = input('Please enter the scores (separate them by a single space): ')
-raw_score = b.split(' ')
+raw_score = b.split(' ')    # all scores
 try:
-    scores = [float(s) for s in raw_score]
+    scores = [int(s) for s in raw_score]  #convert to integers
 except:
     print ('Enter numbers, you FOOL!!')
 if len(scores) != a:
     print ('Enter enough scores, you FOOL!')
 
-best = 1
-min_best = 100
-worst = 1
-max_worst = 0
-for i in scores:
+best = 1    # a flag to indicate all pass
+min_best = 100  # store the min pass score 
+worst = 1   # a floag to indicate no one pass
+max_worst = 0   # store the max failed score
+for i in scores:    # scan all scores 
     if i < 60:
-        best = 0
+        best = 0    # if one failed score, the best set to 0
         if i > max_worst:
-            max_worst = i
-    if i >= 60:
-        worst = 0
+            max_worst = i   # find the max failed score
+    if i >= 60:     
+        worst = 0   #if existing pass score, the worst set to 0
         if i < min_best:
             min_best = i
 scorestr = ''
-for s in sorted(scores):
+for s in sorted(scores):    # sort scores and print
     scorestr += str(s)
     scorestr += ' '
 
-print(scorestr)
+print(scorestr[:len(scorestr)-1])
     
 if best:
     print ('best case')
