cd9b2d743d233444ba46475fc4be84110077f006
wyshih <wyshih@cs.nctu.edu.tw>
2018-08-08 15:35:13 +0800

    add comments

--- 1050305-2.py
+++ 1050305-2.py
@@ -1,4 +1,4 @@
-def turn(R, M, matrix):
+def turn(R, M, matrix):     #turn matrix
     result = [[0]*R for i in range(0, M)]
     #print result
     for i in range(0, R):
@@ -6,7 +6,16 @@ def turn(R, M, matrix):
             result[j][R-i-1] = matrix[i][j]
     #print result
     return result
+'''
+original    after turn      new index
 
+00 01       20 10 00         00 01 02
+10 11   ->  21 11 01   <-->  10 11 12
+20 21
+
+the original columns become row, and the order of original rows are reversed to become new columns
+
+'''
 def reverse(R, M, matrix):
     result = [[0]*M for i in range(0, R)]
     for i in range(0, R):
@@ -15,6 +24,15 @@ def reverse(R, M, matrix):
     #print result
     return result
 
+'''
+original    after reverse
+
+00 01       20 21       
+10 11   ->  10 11     
+20 21       00 01
+
+the columns are not changed, but the order of rows are reversed
+'''
 if __name__ == '__main__':
     R, M, C = input().split(' ')
     R = int(R)
--- 1050305-3.py
+++ 1050305-3.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    line = []
+    line = []   #set a list to record segments if occupied the value of that index is set to 1
     
     segments = int(input())
     
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         if int(S) > int(E):
             print ('Input Error')
             break
-        if len(line) < int(E):
+        if len(line) < int(E):  #if the new segment is longer than record list then extend the record
             line.extend([0]*(int(E)-len(line)))
-        line[int(S):int(E)] = [1]*(int(E)-int(S))
-    print (sum(line))
+        line[int(S):int(E)] = [1]*(int(E)-int(S))   #set the range of segment in reocrd list to 1
+    print (sum(line))   #sum all values in record list and print the value which is the length of all segments
     
