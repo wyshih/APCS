1eeee51978f502795f9b07b48f1f4cc5dc77136a
wyshih <wyshih@cs.nctu.edu.tw>
2018-08-09 09:39:25 +0800

    add comments

--- 1051029-1.py
+++ 1051029-1.py
@@ -1,7 +1,7 @@
 #second Problem 1
-def judge(length):
+def judge(length):  # follow the rules to find what kind of triangle is
     flag = 0
-    length.sort()
+    length.sort()   
     if length[0] + length[1]  <= length[2]:
         return 'No'
     elif pow(length[2], 2) == pow(length[0], 2) + pow(length[1], 2):
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     b = int(b)
     c = int(c)
     length = []
-    if a < 30001 and a > 0 and b < 30001 and b > 0 and c < 30001 and c > 0:
+    if a < 30001 and a > 0 and b < 30001 and b > 0 and c < 30001 and c > 0:     # store input into a list
         length.append(a)
         length.append(b)
         length.append(c)
